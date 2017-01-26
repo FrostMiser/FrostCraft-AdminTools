b0b97f766a2b21667133f2abb6d4e56cb9cf84d3
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-25 20:43:50 -0600

    Change enable and disable message color.

--- src/com/frostcraft/admintools/Main.java
+++ src/com/frostcraft/admintools/Main.java
@@ -1,13 +1,18 @@
 package com.frostcraft.admintools;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public void onEnable() {
-		this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[FrostCraft-AdminTools] Plugin enabled.");
+		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostCraft-AdminTools] Plugin enabled.");
 	}
 	
 	public void onDisable() {
-		this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[FrostCraft-AdminTools] Plugin disabled.");
+		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostCraft-AdminTools] Plugin disabled.");
+	}
+	
 	}
 }
