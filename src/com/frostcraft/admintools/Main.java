3bc4d5824b31d1e0062030c64ea5f371fe334901
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-25 20:38:25 -0600

    Add messages for plugin enable and disable.

--- src/com/frostcraft/admintools/Main.java
+++ src/com/frostcraft/admintools/Main.java
@@ -1,12 +1,13 @@
 package com.frostcraft.admintools;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public void onEnable() {
-		
+		this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[FrostCraft-AdminTools] Plugin enabled.");
 	}
 	
 	public void onDisable() {
-		
+		this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[FrostCraft-AdminTools] Plugin disabled.");
 	}
 }
