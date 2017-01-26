2066eb43094664d3860a87e42c07fbf043fa7d0b
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-25 21:46:51 -0600

    Change help text color.

--- src/com/frostcraft/admintools/Main.java
+++ src/com/frostcraft/admintools/Main.java
@@ -34,25 +34,25 @@ public class Main extends JavaPlugin {
 			if (cmd.getName().equalsIgnoreCase("fcadmin")) {
 				
 				if (args.length <= 0) {
-					p.sendMessage(ChatColor.DARK_PURPLE + "FrostCraft-AdminTools Commands");
-					p.sendMessage(ChatColor.DARK_PURPLE + "==============================");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin kick [P]" + ChatColor.GREEN + " - Kick a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin kickwarn [P]" + ChatColor.GREEN + " - Kick a player and add a warning.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin ban [P]" + ChatColor.GREEN + " - Ban a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin tempban [P]" + ChatColor.GREEN + " - Temp ban a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin mute [P]" + ChatColor.GREEN + " - Mute a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin unmute [P]" + ChatColor.GREEN + " - UnMute a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin vanish" + ChatColor.GREEN + " - Vanish.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin vanish [P]" + ChatColor.GREEN + " - Vanish only from a specific player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin seeinv [P]" + ChatColor.GREEN + " - View a player inventory.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin seeinvender [P]" + ChatColor.GREEN + " - View a player ender inventory.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin tpl [P]" + ChatColor.GREEN + " - Teleport to a player.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin gm" + ChatColor.GREEN + " - Change your gamemode.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin time" + ChatColor.GREEN + " - Set the world time.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin togglephysics" + ChatColor.GREEN + " - Turn physics on or off.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin addpoint [Name]" + ChatColor.GREEN + " - Add an admin waypoint.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin listpoints" + ChatColor.GREEN + " - List admin waypoints.");
-					p.sendMessage(ChatColor.DARK_PURPLE + "fcadmin point [Name]" + ChatColor.GREEN + " - Teleport to a waypoint.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "FrostCraft-AdminTools Commands");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "==============================");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin kick [P] [Msg]" + ChatColor.GREEN + " - Kick a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin kickwarn [P]" + ChatColor.GREEN + " - Kick a player and add a warning.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin ban [P]" + ChatColor.GREEN + " - Ban a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin tempban [P]" + ChatColor.GREEN + " - Temp ban a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin mute [P]" + ChatColor.GREEN + " - Mute a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin unmute [P]" + ChatColor.GREEN + " - UnMute a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin vanish" + ChatColor.GREEN + " - Vanish.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin vanish [P]" + ChatColor.GREEN + " - Vanish only from a specific player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin seeinv [P]" + ChatColor.GREEN + " - View a player inventory.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin seeinvender [P]" + ChatColor.GREEN + " - View a player ender inventory.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin tpl [P]" + ChatColor.GREEN + " - Teleport to a player.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin gm" + ChatColor.GREEN + " - Change your gamemode.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin time" + ChatColor.GREEN + " - Set the world time.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin togglephysics" + ChatColor.GREEN + " - Turn physics on or off.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin addpoint [Name]" + ChatColor.GREEN + " - Add an admin waypoint.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin listpoints" + ChatColor.GREEN + " - List admin waypoints.");
+					p.sendMessage(ChatColor.LIGHT_PURPLE + "fcadmin point [Name]" + ChatColor.GREEN + " - Teleport to a waypoint.");
 				}
 			}
 		}
