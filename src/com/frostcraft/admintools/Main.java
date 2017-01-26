f0e5a5af1945f2015e2bbdfba013d058bedbbab2
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-25 20:44:02 -0600

    Add fcadmin command.

--- src/com/frostcraft/admintools/Main.java
+++ src/com/frostcraft/admintools/Main.java
@@ -14,5 +14,28 @@ public class Main extends JavaPlugin {
 		this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[FrostCraft-AdminTools] Plugin disabled.");
 	}
 	
+	
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	{
+		Player p;
+		try {
+			p = (Player) sender;	
+		}
+		catch (Exception e) {
+			p = null;
+			sender.sendMessage(ChatColor.RED + "[FrostCraft-AdminTools] This command must be issued by a player.");
+			return true;
+		}
+		
+		//fcadmin command
+		if (p != null) {
+			if (cmd.getName().equalsIgnoreCase("fcadmin")) {
+				
+				
+			}
+		}
+		return true;
 	}
 }
