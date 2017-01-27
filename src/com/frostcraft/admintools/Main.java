f07a3e9c573b300aed41fb0370a07b4528da1843
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-26 21:48:47 -0600

    Cleanup.

--- src/com/frostcraft/admintools/Main.java
+++ src/com/frostcraft/admintools/Main.java
@@ -5,8 +5,6 @@ import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
